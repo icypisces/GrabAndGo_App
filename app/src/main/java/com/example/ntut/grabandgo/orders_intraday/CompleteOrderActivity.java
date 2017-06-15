a6fe3f7abb35b8b7ef967bd50763c59abfcb365d
PC219-25\NTUT <icypisces318@gmail.com>
2017-06-15 18:29:05 +0800

    123

---
+++ app/src/main/java/com/example/ntut/grabandgo/orders_intraday/CompleteOrderActivity.java
@@ -0,0 +1,15 @@
+package com.example.ntut.grabandgo.orders_intraday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ntut.grabandgo.R;
+
+public class CompleteOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complete_order);
+    }
+}

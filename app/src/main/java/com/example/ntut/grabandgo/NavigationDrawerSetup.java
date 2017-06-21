87370b21552f781f34f43d0d6d92a50908781f99
LAPTOP-C1O370F4\icypi <icypisces318@gmail.com>
2017-06-22 01:30:14 +0800

    Drawer因不明原因未觸發Drawer內項目點擊事件...待處理

--- app/src/main/java/com/example/ntut/grabandgo/NavigationDrawerSetup.java
+++ app/src/main/java/com/example/ntut/grabandgo/NavigationDrawerSetup.java
@@ -1,11 +1,105 @@
 package com.example.ntut.grabandgo;
 
+import android.content.Intent;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
 
-public class NavigationDrawerSetup {
+import com.example.ntut.grabandgo.login_logout_register.LoginActivity;
+import com.example.ntut.grabandgo.login_logout_register.RegisterActivity;
 
-    public void configureDrawer(){
+public class NavigationDrawerSetup extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private FrameLayout frameLayout;
+    protected NavigationView navigationView;    //其他Activity共用
+    protected Toolbar toolbar;
+
+
+    @Override
+    public void setContentView(@LayoutRes int layoutResID) {
+        findViews();
+        getLayoutInflater().inflate(layoutResID,drawerLayout,true);
+        //參數1 - resource  //參數2 - 成為哪個view的child view
+        //參數3 - 最後一個參數 attachToRoot 是 inflate 的 view 是否要 attach 到 root view，
+        // 這會影響到回傳的是哪個 view。假如 attachToRoot 為 true，
+        // 則最後回傳的 view 為 root view (就是第二個參數的 view)；反之就是回傳 inflate 的 view。
+        super.setContentView(drawerLayout);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setUpNavigation();
+    }
+
+
+    public void setUpNavigation() {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(true);          //設定被點選的Item有被點選的效果(灰色)
+                drawerLayout.closeDrawers();    //強制在點選後抽屜會關掉
+                Intent intent = null;
+                switch (item.getItemId()){
+                    case R.id.item_login:
+                        intent.setClass(NavigationDrawerSetup.this,LoginActivity.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.item_register:
+                        intent.setClass(NavigationDrawerSetup.this,RegisterActivity.class);
+                        startActivity(intent);
+                        break;
+                }
+
+                return false;
+            }
+        });
 
     }
+
+
+    private void findViews() {
+        drawerLayout = (DrawerLayout) getLayoutInflater().inflate(R.layout.navigation_drawer_setup, null);
+        frameLayout = (FrameLayout) drawerLayout.findViewById(R.id.content_frame);
+        navigationView = (NavigationView) drawerLayout.findViewById(R.id.navigation_view);
+
+    }
+
+
+    //用以設置toolbar
+    public void setUpToolBar(){
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);//左側
+            }
+        });
+
+        //被點擊的Icon設定會轉動
+        ActionBarDrawerToggle actionBarDrawerToggle =
+                new ActionBarDrawerToggle(this,drawerLayout,toolbar,
+                        R.string.drawer_open,R.string.drawer_close){
+                    @Override
+                    public void onDrawerOpened(View drawerView) {
+                        super.onDrawerOpened(drawerView);
+                    }
+
+                    @Override
+                    public void onDrawerClosed(View drawerView) {
+                        super.onDrawerClosed(drawerView);
+                    }
+        };
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();  //讓ActionBar 中的返回箭號置換成Drawer 的三條線圖示
+
+    }
+
 }

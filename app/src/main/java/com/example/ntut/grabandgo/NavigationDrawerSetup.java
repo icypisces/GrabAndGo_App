22824a9af908ff946c2960ce7813c8fe5c9c1fca
LAPTOP-C1O370F4\icypi <icypisces318@gmail.com>
2017-06-21 20:59:50 +0800

    通用Drawer測試中

---
+++ app/src/main/java/com/example/ntut/grabandgo/NavigationDrawerSetup.java
@@ -0,0 +1,11 @@
+package com.example.ntut.grabandgo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NavigationDrawerSetup {
+
+    public void configureDrawer(){
+
+    }
+}

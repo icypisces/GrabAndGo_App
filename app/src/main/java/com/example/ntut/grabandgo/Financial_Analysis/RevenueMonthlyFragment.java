2799eb3a428315f457d5a31d747ee3e3b8d18f30
PC219-25\NTUT <icypisces318@gmail.com>
2017-07-06 18:20:03 +0800

    月營收折線圖終於出生了.......

--- app/src/main/java/com/example/ntut/grabandgo/Financial_Analysis/RevenueMonthlyFragment.java
+++ app/src/main/java/com/example/ntut/grabandgo/Financial_Analysis/RevenueMonthlyFragment.java
@@ -1,7 +1,5 @@
 package com.example.ntut.grabandgo.Financial_Analysis;
 
-import android.content.Context;
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.support.annotation.Nullable;
@@ -11,23 +9,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
-import com.example.ntut.grabandgo.MainActivity;
 import com.example.ntut.grabandgo.R;
 import com.example.ntut.grabandgo.orders_daily.BaseFragment;
-import com.example.ntut.grabandgo.orders_daily.DailyOrdersActivity;
-import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.components.YAxis;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 import java.text.DateFormat;
@@ -39,7 +29,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 
 public class RevenueMonthlyFragment extends BaseFragment {
@@ -47,28 +36,20 @@ public class RevenueMonthlyFragment extends BaseFragment {
     private static final String TAG = "RevenueMonthlyFragment";
     private Spinner spYearSelect, spMonthSelect;
     private Button btSelectMonth;
+    private EditText edRevenueTotal;
     private int today_year, today_month;
     private final int months = 12;
     private LineChart monthlyLineChart;
     private String selectMonth;
     private List<OrderItem> orderItemList = null;
 
-
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.financial_fragment_revenue_monthly,container,false);  //取得layout檔
-
         findView(view);
         setSpinners();
-
-//        monthlyLineChart.setData(getLineData());
-//        monthlyLineChart.notifyDataSetChanged();
-//        monthlyLineChart.invalidate();
-
-
         return view;
     }
 
@@ -76,6 +57,7 @@ public class RevenueMonthlyFragment extends BaseFragment {
         spYearSelect = (Spinner) view.findViewById(R.id.spYearSelect);
         spMonthSelect = (Spinner) view.findViewById(R.id.spMonthSelect);
         btSelectMonth = (Button) view.findViewById(R.id.btSelectMonth);
+        edRevenueTotal = (EditText) view.findViewById(R.id.edRevenueTotal);
         monthlyLineChart = (LineChart) view.findViewById(R.id.monthlyLineChart);
     }
 
@@ -121,8 +103,10 @@ public class RevenueMonthlyFragment extends BaseFragment {
                 selectMonth = spYearSelect.getSelectedItem().toString().trim() + "-"
                         + spMonthSelect.getSelectedItem().toString().trim();
                 Object[] xAndyData = getDateData(orderItemList);
+                edRevenueTotal.setText(Integer.toString((int)xAndyData[1]));
                 try {
-                    monthlyLineChart.setData(getLineData((Map<Integer, Float>) xAndyData[0], (String) xAndyData[2]));    //( (X,Y) , selectMonth )
+                    monthlyLineChart.setData(getLineData(
+                            (Map<Integer, Float>) xAndyData[0], (String) xAndyData[2]));    //( (X,Y) , selectMonth )
                 } catch (ParseException e) {
                     e.printStackTrace();
                 }
@@ -137,16 +121,16 @@ public class RevenueMonthlyFragment extends BaseFragment {
             orderItemList = (List<OrderItem>) bundle.getSerializable("orderItemList");
 
             Object[] xAndyData = getDateData(orderItemList);
+            edRevenueTotal.setText(Integer.toString((int)xAndyData[1]));
             try {
-                monthlyLineChart.setData(getLineData((Map<Integer, Float>) xAndyData[0], (String) xAndyData[2]));    //( (X,Y) , selectMonth )
+                monthlyLineChart.setData(getLineData(
+                        (Map<Integer, Float>) xAndyData[0], (String) xAndyData[2]));    //( (X,Y) , selectMonth )
             } catch (ParseException e) {
                 e.printStackTrace();
             }
             monthlyLineChart.notifyDataSetChanged();
             monthlyLineChart.invalidate();
-
         }
-
     }
 
     private Object[] getDateData(List<OrderItem> orderItemList){
@@ -154,7 +138,7 @@ public class RevenueMonthlyFragment extends BaseFragment {
         selectMonth = spYearSelect.getSelectedItem().toString().trim() + "-"
                 + spMonthSelect.getSelectedItem().toString().trim();
         for (int i = 0; i < orderItemList.size(); i++) {
-            String[] notes = orderItemList.get(i).getItem_note().split("||/");
+            String[] notes = orderItemList.get(i).getItem_note().split("\\/");
             String orderMonth = notes[1];
             if ( orderMonth.equals(selectMonth)) {
                 orderItemListMonthly.add(orderItemList.get(i));
@@ -166,28 +150,35 @@ public class RevenueMonthlyFragment extends BaseFragment {
         int RevenueTotalMonthly = 0;
         Map<Integer, Float> map = new HashMap<>();
         for (int i = 0; i < orderItemListMonthly.size(); i++) {
-            xData[i] = Integer.parseInt(orderItemList.get(i).getItem_note().split("/")[0]);//有收入的日期
+            xData[i] = Integer.parseInt(orderItemList.get(i).getItem_note().split("\\/")[0]);//有收入的日期
             yData[i] = orderItemListMonthly.get(i).getItem_price();         //該日期收入
             map.put(xData[i], yData[i]);
             RevenueTotalMonthly += yData[i];                                //累加收入
         }
-
         Object[] xAndyData = {map, RevenueTotalMonthly, selectMonth};
-
         return xAndyData;
     }
 
     /* 將 DataSet 資料整理好後，回傳 LineData */
     private LineData getLineData(Map<Integer, Float> map, String selectMonth) throws ParseException {
         //取得查詢當月的日數
-        DateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
-        Date date = sdf.parse(selectMonth + "/01");
+        DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = sdf.parse(selectMonth + "-1");
         Calendar cal = Calendar.getInstance();
         cal.setTime(date);
         int DATA_COUNT = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
 
         // LineDataSet(List<Entry> 資料點集合, String 類別名稱)
         LineDataSet dataSetA = new LineDataSet( getChartData(DATA_COUNT, map), selectMonth);
+        dataSetA.setColor(Color.rgb(252, 190, 128));
+        dataSetA.setLineWidth(2f);
+        dataSetA.setDrawCircleHole(true);
+        dataSetA.setCircleColor(Color.rgb(255, 156, 58));       //設置空心圓顏色
+        dataSetA.setCircleRadius(10f);                           //設置空心圓半徑
+        dataSetA.setCircleColorHole(Color.rgb(255, 255, 255));   //設置內圓顏色
+        dataSetA.setCircleRadius(5f);                            //設置內圓半徑
+        dataSetA.setDrawFilled(true);                            //線的陰影
+
 //        LineDataSet dataSetB = new LineDataSet( getChartData(DATA_COUNT, 2), "B");
 
         List<ILineDataSet> dataSets = new ArrayList<>();
@@ -198,7 +189,6 @@ public class RevenueMonthlyFragment extends BaseFragment {
         LineData data = new LineData(getLabels(DATA_COUNT), dataSets);
 
         return data;
-
     }
 
     /* 取得 List<Entry> 的資料給 DataSet */
@@ -220,7 +210,6 @@ public class RevenueMonthlyFragment extends BaseFragment {
 
     /* 取得 XVals Labels 給 LineData */
     private List<String> getLabels(int count){
-
         List<String> chartLabels = new ArrayList<>();
         for(int i=0;i<count;i++) {
             chartLabels.add((i+1) + "日");
@@ -228,10 +217,8 @@ public class RevenueMonthlyFragment extends BaseFragment {
         return chartLabels;
     }
 
-
     @Override
     protected void lazyLoad() {
 
     }
-
 }
